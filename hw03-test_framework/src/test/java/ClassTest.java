4238af212b0acecb094ba684125dfd9c4f9d4cf1
Ilya Rogatkin <toch.in.go@googlemail.com>
2020-04-20 20:25:14 +0300

    add annotations and class test

---
+++ hw03-test_framework/src/test/java/ClassTest.java
@@ -0,0 +1,14 @@
+import ru.otus.annotations.After;
+import ru.otus.annotations.Before;
+import ru.otus.annotations.Test;
+
+public class ClassTest {
+    @Before
+    public void init(){}
+
+    @Test
+    public void go(){}
+
+    @After
+    public void stop(){}
+}
