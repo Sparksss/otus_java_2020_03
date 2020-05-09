53a773eb569e1d3fcf376d321c49229a2678251f
Ilya Rogatkin <toch.in.go@googlemail.com>
2020-05-09 18:59:55 +0300

    add dep in general build conf and version

--- hw05-auto_logger/src/main/java/ru/otus/AutoLogger.java
+++ hw05-auto_logger/src/main/java/ru/otus/AutoLogger.java
@@ -4,11 +4,6 @@
 
 package ru.otus;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.TreeSet;
-
 public class AutoLogger {
     public static void main(String[] args) {
         new TestLogging().calculation(10);
