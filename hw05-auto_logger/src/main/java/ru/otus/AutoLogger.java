016de5064936b5ebf9c86e330408a4325eb3d46f
Ilya Rogatkin <toch.in.go@googlemail.com>
2020-05-09 18:22:09 +0300

    add test class with method and add depend ASM in gradle config

--- hw05-auto_logger/src/main/java/ru/otus/AutoLogger.java
+++ hw05-auto_logger/src/main/java/ru/otus/AutoLogger.java
@@ -4,8 +4,13 @@
 
 package ru.otus;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.TreeSet;
+
 public class AutoLogger {
     public static void main(String[] args) {
-
+        new TestLogging().calculation(10);
     }
 }
---
+++ hw05-auto_logger/src/main/java/ru/otus/TestLogging.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Ilya Rogatkin, May 2020
+ */
+
+package ru.otus;
+
+import ru.otus.annotations.Log;
+
+public class TestLogging {
+
+    @Log
+    public void calculation(int param) {
+
+    }
+}
