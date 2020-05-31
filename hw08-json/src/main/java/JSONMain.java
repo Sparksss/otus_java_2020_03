8f9adcf24fc410ba8b6c5391fdc2c4022e870b92
Ilya Rogatkin <toch.in.go@googlemail.com>
2020-05-31 19:12:52 +0300

    add json dependencies

---
+++ hw08-json/src/main/java/JSONMain.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Ilya Rogatkin, May 2020
+ */
+
+public class JSONMain {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ hw08-json/src/main/java/MyJSON/MyJSON.java
@@ -0,0 +1,18 @@
+/**
+ * Created by Ilya Rogatkin, May 2020
+ */
+
+package MyJSON;
+
+import javax.json.JsonArray;
+import javax.json.JsonNumber;
+import javax.json.JsonObject;
+import javax.json.JsonString;
+import javax.json.JsonStructure;
+import javax.json.JsonValue;
+
+public class MyJSON {
+    public void toJson() {
+
+    }
+}
