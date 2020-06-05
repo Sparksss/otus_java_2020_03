e9cc2034bb9192ac586cff855dc6ee623a5e21c4
Ilya Rogatkin <toch.in.go@googlemail.com>
2020-06-05 12:22:40 +0300

    copy files from study repo, config for h2

--- buildSrc/src/main/java/Versions.java
+++ buildSrc/src/main/java/Versions.java
@@ -1,5 +1,7 @@
 public interface Versions {
     String guava = "28.2-jre";
+    String jol = "0.10";
     String asm = "8.0.1";
-    String junit = "5.3.1";
+    String glassfishJson = "1.1.4";
+    String jaxbApi = "2.3.1";
 }
