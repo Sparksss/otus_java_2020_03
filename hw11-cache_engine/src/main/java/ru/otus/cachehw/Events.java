94c60431c33845842e915ffc9f988ae1464d3f4a
i.rogatkin <i.rogatkin@medpoint24.ru>
2020-07-17 12:52:07 +0300

    implemenst MyCache

---
+++ hw11-cache_engine/src/main/java/ru/otus/cachehw/Events.java
@@ -0,0 +1,5 @@
+package ru.otus.cachehw;
+
+public enum Events {
+    CREATED, UPDATED, READ, DELETE
+}
--- hw11-cache_engine/src/main/java/ru/otus/cachehw/MyCache.java
+++ hw11-cache_engine/src/main/java/ru/otus/cachehw/MyCache.java
@@ -1,38 +1,67 @@
 package ru.otus.cachehw;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.WeakHashMap;
 
-/**
- * @author sergey
- * created on 14.12.18.
- */
 public class MyCache<K, V> implements HwCache<K, V> {
-//Надо реализовать эти методы
 
-    private WeakHashMap<K, V> weakHashMap = new WeakHashMap<>();
+    private final Map<K, V> weakHashMap;
+
+    private final Map<String, HwListener<K, V>> listeners;
+
+    private MyCache() {
+        this.weakHashMap = new WeakHashMap<>();
+        this.listeners = new HashMap<>();
+    }
+
+    private MyCache(int count) {
+        this.weakHashMap = new WeakHashMap<>(count);
+        this.listeners = new HashMap<>();
+    }
+
+    static MyCache getInstance() {
+        return new MyCache<>();
+    }
+
+    static MyCache getInstance(int count) {
+        return new MyCache<>(count);
+    }
 
     @Override
     public void put(K key, V value) {
         this.weakHashMap.put(key, value);
+        if(this.listeners.containsKey("CREATE")) {
+            HwListener<K,V> handler = this.listeners.get("CREATE");
+            handler.notify(key, value, "CREATE");
+        }
     }
 
     @Override
     public void remove(K key) {
-        this.weakHashMap.remove(key);
+        V value = this.weakHashMap.remove(key);
+        if(this.listeners.containsKey("DELETE")) {
+            HwListener<K,V> handler = this.listeners.get("DELETE");
+            handler.notify(key, value, "DELETE");
+        }
     }
 
     @Override
     public V get(K key) {
-        return this.weakHashMap.get(key);
+        V value = this.weakHashMap.get(key);
+        if(this.listeners.containsKey("READ")) {
+            HwListener<K,V> handler = this.listeners.get("READ");
+            handler.notify(key, value, "READ");
+        }
+        return value;
     }
 
     @Override
     public void addListener(HwListener<K, V> listener) {
-            
     }
 
     @Override
     public void removeListener(HwListener<K, V> listener) {
-
+        this.listeners.remove(listener);
     }
 }
