fe943e30354f5c781f20a9322787455c8d0b0c68
Ilya Rogatkin <toch.in.go@googlemail.com>
2020-08-23 23:54:15 +0300

    rename method names

--- hw14-spring_MVC/src/main/java/ru/otus/core/service/DBServiceUser.java
+++ hw14-spring_MVC/src/main/java/ru/otus/core/service/DBServiceUser.java
@@ -3,15 +3,14 @@ package ru.otus.core.service;
 import ru.otus.core.model.User;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface DBServiceUser {
 
     long saveUser(User user);
 
-    Optional<User> getUser(long id);
+    User getUserById(long id);
 
-    User getUserByLogin(String login);
+    User getUserByName(String name);
 
     List<User> getAllUsers();
 
