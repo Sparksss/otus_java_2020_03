3fb58cd0284bb89af8d625a36d0578578477ebff
Ilya Rogatkin <toch.in.go@googlemail.com>
2020-08-24 18:55:07 +0300

    implement user service

--- hw14-spring_MVC/src/main/java/ru/otus/services/UserServiceImpl.java
+++ hw14-spring_MVC/src/main/java/ru/otus/services/UserServiceImpl.java
@@ -1,27 +1,38 @@
 package ru.otus.services;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.otus.core.dao.UserDao;
 import ru.otus.core.model.User;
+import ru.otus.repository.UserRepository;
 
 import java.util.List;
 
+@Service
+@RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
+
+    UserRepository userRepository;
+    UserDao userDao;
+
     @Override
     public List<User> findAll() {
-        return null;
+        return userRepository.findAll();
     }
 
     @Override
     public User findById(long id) {
-        return null;
+        return userDao.findById(id);
     }
 
     @Override
     public User findByName(String name) {
-        return null;
+        return userDao.findByName(name);
     }
 
     @Override
     public User save(User user) {
-        return null;
+        long id = userDao.insertUser(user);
+        return userDao.findById(id);
     }
 }
