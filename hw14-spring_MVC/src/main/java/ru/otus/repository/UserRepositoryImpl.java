376f51f7977355813eb67357ba661560a378c5a0
Ilya Rogatkin <toch.in.go@googlemail.com>
2020-08-24 18:56:00 +0300

    add user service to repository

--- hw14-spring_MVC/src/main/java/ru/otus/repository/UserRepositoryImpl.java
+++ hw14-spring_MVC/src/main/java/ru/otus/repository/UserRepositoryImpl.java
@@ -1,12 +1,14 @@
 package ru.otus.repository;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
 import ru.otus.core.dao.UserDao;
 import ru.otus.core.model.User;
 import ru.otus.core.service.DBServiceUser;
 
 import java.util.List;
 
+@Repository
 @RequiredArgsConstructor
 public class UserRepositoryImpl implements UserRepository {
 
