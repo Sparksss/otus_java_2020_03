c69dae49a840f1197aec4c1224b79faca64ac2bb
Ilya Rogatkin <toch.in.go@googlemail.com>
2020-08-24 18:56:56 +0300

    add user service in controller

--- hw14-spring_MVC/src/main/java/ru/otus/controllers/UserController.java
+++ hw14-spring_MVC/src/main/java/ru/otus/controllers/UserController.java
@@ -1,19 +1,24 @@
 package ru.otus.controllers;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
 import ru.otus.core.model.User;
-import ru.otus.repository.UserRepository;
-import ru.otus.repository.UserRepositoryImpl;
+import ru.otus.services.UserService;
 
 import java.util.List;
 
 @Controller
+@RequiredArgsConstructor
 public class UserController {
+
+    UserService userService;
+
     @GetMapping({"/", "/user/list"})
-    public String userListView() {
-        UserRepository repository = new UserRepositoryImpl();
-        List<User> users = repository.findAll();
+    public String userListView(Model model) {
+        List<User> users = userService.findAll();
+        model.addAttribute("users", users);
         return "userList.html";
     }
 }
