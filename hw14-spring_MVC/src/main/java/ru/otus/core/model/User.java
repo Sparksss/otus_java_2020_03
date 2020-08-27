35e214e58dae25d1901ac91dfa0f103089bbf08d
Ilya Rogatkin <toch.in.go@googlemail.com>
2020-08-27 11:05:06 +0300

    fix annotation

--- hw14-spring_MVC/src/main/java/ru/otus/core/model/User.java
+++ hw14-spring_MVC/src/main/java/ru/otus/core/model/User.java
@@ -1,36 +1,24 @@
 package ru.otus.core.model;
 
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 import javax.persistence.*;
 
 @Entity
 @Table(name = "users")
 @NoArgsConstructor
+@Data
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
-    @Getter
-    @Setter
     private long id;
 
     @Column(name = "name", unique = true)
-    @Getter
-    @Setter
     private String name;
 
     public User(String name) {
         this.name = name;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", name='" + name + '\'' + '}';
     }
 }
