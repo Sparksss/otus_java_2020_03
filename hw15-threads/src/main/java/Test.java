986f69f0aa7dfe79304bbb75d3e5a899f9f4aa50
Ilya Rogatkin <toch.in.go@googlemail.com>
2020-08-27 10:23:20 +0300

    add main logic in test class

--- hw15-threads/src/main/java/Test.java
+++ hw15-threads/src/main/java/Test.java
@@ -1,21 +1,53 @@
 /**
- * Created by ilya on Aug , 2020
+ * Created by ilya on Aug, 2020
  */
+
+
 public class Test implements Runnable {
+
+    private final int MAX = 10;
+
     private int number = 0;
+
+    private boolean isIncrement = true;
 
     @Override
     public void run() {
-        increment(Thread.currentThread().getName());
-    }
-
-    private void increment(String name) {
         for(int i = 0; i < 10; i++) {
-            System.out.println(name + "->" + i);
+            calculate();
         }
     }
 
-    public int getNumber() {
-        return number;
+    private synchronized void calculate() {
+        if(this.number < this.MAX && this.isIncrement) {
+            increment();
+        } else {
+            this.isIncrement = false;
+            decrement();
+        }
+        this.notifyAll();
+        if(!this.isIncrement && this.number == 0) return;
+        showNumber(Thread.currentThread().getName());
+            while (true) {
+                try {
+                    this.wait();
+                    break;
+                } catch (InterruptedException exception) {
+                    System.out.println(exception.getMessage());
+                }
+        }
+    }
+
+
+    private void increment() {
+        ++this.number;
+    }
+
+    private void decrement() {
+        --this.number;
+    }
+
+    private void showNumber (String threadName) {
+        System.out.println(threadName + " -> " + number);
     }
 }
