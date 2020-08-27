2acd010aba13c0b49dabb06865bdcab14b206ba2
Ilya Rogatkin <toch.in.go@googlemail.com>
2020-08-27 10:44:54 +0300

    add sleep because always need first start thread 1

--- hw15-threads/src/main/java/Main.java
+++ hw15-threads/src/main/java/Main.java
@@ -6,7 +6,16 @@ public class Main {
         Test test = new Test();
         Thread thread = new Thread(test);
         Thread thread1 = new Thread(test);
+        thread.setName("Поток 1");
+        thread1.setName("Поток 2");
         thread.start();
+
+        try {
+            Thread.sleep(1);
+        } catch (InterruptedException exception) {
+            System.out.println(exception.getMessage());
+        }
+
         thread1.start();
     }
 }
