08963dd1ce4a62af7e89852bf3fe122f27b1b855
Ilya Rogatkin <toch.in.go@googlemail.com>
2020-08-30 13:26:35 +0300

    fix order start thread's

--- hw15-threads/src/main/java/Main.java
+++ hw15-threads/src/main/java/Main.java
@@ -1,6 +1,10 @@
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.concurrent.ExecutorService;
+
+import static java.util.concurrent.Executors.newFixedThreadPool;
+
 /**
  * Created by ilya on Aug, 2020
  */
@@ -8,20 +12,9 @@ public class Main {
 
     final private static Logger logger = LoggerFactory.getLogger(Main.class);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Test test = new Test();
-        Thread thread = new Thread(test);
-        Thread thread1 = new Thread(test);
-        thread.setName("Поток 1");
-        thread1.setName("Поток 2");
-        thread.start();
-
-        try {
-            Thread.sleep(1);
-        } catch (InterruptedException exception) {
-            logger.error(exception.getMessage());
-        }
-
-        thread1.start();
+        new Thread(() -> test.calculate("Thread-1")).start();
+        new Thread(() -> test.calculate("Thread-2")).start();
     }
 }
--- hw15-threads/src/main/java/Test.java
+++ hw15-threads/src/main/java/Test.java
@@ -1,12 +1,10 @@
-/**
- * Created by ilya on Aug, 2020
- */
-
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Test implements Runnable {
+/**
+ * Created by ilya on Aug, 2020
+ */
+public class Test {
 
     private final int MAX = 10;
 
@@ -16,35 +14,35 @@ public class Test implements Runnable {
 
     private boolean isNeedCalculate = true;
 
+    private String lastThreadName = "Thread-2";
+
     final private static Logger logger = LoggerFactory.getLogger(Test.class);
 
-    @Override
-    public void run() {
-        for(int i = 0; i < 20; i++) {
-            calculate();
-        }
-    }
 
-    private synchronized void calculate() {
-        if(this.isNeedCalculate) {
-            if(this.number < this.MAX && this.isIncrement) {
-                increment();
-            } else {
-                this.isIncrement = false;
-                decrement();
-            }
-        }
-        this.isNeedCalculate = !this.isNeedCalculate;
-        this.notifyAll();
-        if(!this.isIncrement && this.number == 0) return;
-        showNumber();
-            while (true) {
+    public synchronized void calculate(String threadName) {
+        for (int i = 0; i < 19; i++) {
+
+            while (this.lastThreadName.equals(threadName)) {
                 try {
                     this.wait();
                     break;
                 } catch (InterruptedException exception) {
                     logger.error(exception.getMessage());
                 }
+            }
+
+            if (this.isNeedCalculate) {
+                if (this.number < this.MAX && this.isIncrement) {
+                    increment();
+                } else {
+                    this.isIncrement = false;
+                    decrement();
+                }
+            }
+            showNumber();
+            this.lastThreadName = threadName;
+            this.isNeedCalculate = !this.isNeedCalculate;
+            this.notifyAll();
         }
     }
 
