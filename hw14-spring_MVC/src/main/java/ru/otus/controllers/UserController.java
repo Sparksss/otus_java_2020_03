a162f5a7b3a13aafa6b5698bb97fe99e02258abd
Ilya Rogatkin <toch.in.go@googlemail.com>
2020-09-05 00:27:41 +0300

    add autowired and find all

--- hw14-spring_MVC/src/main/java/ru/otus/controllers/UserController.java
+++ hw14-spring_MVC/src/main/java/ru/otus/controllers/UserController.java
@@ -1,28 +1,36 @@
 package ru.otus.controllers;
 
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
-import ru.otus.core.model.User;
+import ru.otus.domain.User;
 import ru.otus.services.UserService;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
-@RequiredArgsConstructor
 public class UserController {
 
     private UserService userService;
 
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping({"/", "/user/list"})
     public String userListView(Model model) {
-        List<User> users = new ArrayList<>();
-        User user = new User();
-        user.setName("John");
-        users.add(user);
+        List<User> users = userService.findAll();
         model.addAttribute("users", users);
         return "userList.html";
     }
+
+    @GetMapping("/user/create")
+    public String userCreateView(Model model) {
+        model.addAttribute("user", new User());
+        return "userCreate.html";
+    }
+
 }
