077c166240c0fc67412abe620082962773aa7ee9
Ilya Rogatkin <toch.in.go@googlemail.com>
2020-09-05 00:31:13 +0300

    add save, delete, getById, update methods

--- hw14-spring_MVC/src/main/java/ru/otus/repository/UserRepository.java
+++ hw14-spring_MVC/src/main/java/ru/otus/repository/UserRepository.java
@@ -1,9 +1,13 @@
 package ru.otus.repository;
 
-import ru.otus.core.model.User;
+import ru.otus.domain.User;
 
 import java.util.List;
 
 public interface UserRepository {
-    List<User> findAll();
+    List<User> getAll();
+    void save(User user);
+    void delete(User user);
+    User getById(long id);
+    void update(User user);
 }
