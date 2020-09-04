0b40600fc06d1d2d4dbed8712aa547ecb65e9dc0
Ilya Rogatkin <toch.in.go@googlemail.com>
2020-09-05 00:30:09 +0300

    add edit user, remove find by name

--- hw14-spring_MVC/src/main/java/ru/otus/services/UserService.java
+++ hw14-spring_MVC/src/main/java/ru/otus/services/UserService.java
@@ -1,12 +1,12 @@
 package ru.otus.services;
 
-import ru.otus.core.model.User;
+import ru.otus.domain.User;
 
 import java.util.List;
 
 public interface UserService {
     List<User> findAll();
     User findById(long id);
-    User findByName(String name);
     User save(User user);
+    void editUser(User user);
 }
