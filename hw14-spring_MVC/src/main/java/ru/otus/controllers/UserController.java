3eec9ea536422e0edaa54995dd33d4ac890b7941
Ilya Rogatkin <toch.in.go@googlemail.com>
2020-09-05 16:35:29 +0300

    ""/" remove root path, add save new user, delete and edit exists

--- hw14-spring_MVC/src/main/java/ru/otus/controllers/UserController.java
+++ hw14-spring_MVC/src/main/java/ru/otus/controllers/UserController.java
@@ -4,10 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import ru.otus.domain.User;
 import ru.otus.services.UserService;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -20,7 +21,7 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping({"/", "/user/list"})
+    @GetMapping({"/user/list"})
     public String userListView(Model model) {
         List<User> users = userService.findAll();
         model.addAttribute("users", users);
@@ -31,6 +32,31 @@ public class UserController {
     public String userCreateView(Model model) {
         model.addAttribute("user", new User());
         return "userCreate.html";
+    }
+
+    @PostMapping("/user/save")
+    public String addUser(@RequestParam String name) {
+        userService.save(new User(name));
+        return "redirect:" + "/user/list";
+    }
+
+    @GetMapping("/user/delete")
+    public String delete(@RequestParam int id) {
+        userService.deleteUser(id);
+        return "redirect:" + "/user/list";
+    }
+
+    @GetMapping("/user/edit")
+    public String edit(@RequestParam int id, Model model) {
+        User user = userService.findById(id);
+        model.addAttribute("user", user);
+        return "userEdit.html";
+    }
+
+    @PostMapping("/user/update")
+    public String update(@RequestParam int id, @RequestParam String name) {
+        userService.update(id, name);
+        return "redirect:" + "/user/list";
     }
 
 }
