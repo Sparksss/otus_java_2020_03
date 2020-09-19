344aeb4f43f4d37c383e03f7365519bc9effd73c
Ilya Rogatkin <toch.in.go@googlemail.com>
2020-09-19 23:56:31 +0300

    add additional fields for prices

--- stock-analyzer/src/main/resources/scheme.sql
+++ stock-analyzer/src/main/resources/scheme.sql
@@ -1,20 +1,23 @@
-create table companies
+CREATE TABLE IF NOT EXISTS companies
 (
 	id serial
 		constraint companies_pk
 			primary key,
 	name varchar(200) not null,
-	description text,
-	establishment_date date
+	symbol varchar(10) not null unique,
+	description text
 );
 
-create table stocks_price
+CREATE TABLE IF NOT EXISTS stocks_price
 (
 	id serial
 		constraint stocks_price_pk
 			primary key,
 	company_id int references companies (id),
 	date date not null,
-	open_price int,
-	close_price int
+	open double,
+	close double,
+	hight double,
+	low double,
+	volume int
 );
