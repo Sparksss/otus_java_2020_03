b2f272bd7027f89af5bf5025f7e45398fa0b3872
Ilya Rogatkin <toch.in.go@googlemail.com>
2020-09-19 23:57:41 +0300

    change url, add dao

--- stock-analyzer/src/main/java/springboot/tasks/ScheduleUpdater.java
+++ stock-analyzer/src/main/java/springboot/tasks/ScheduleUpdater.java
@@ -4,6 +4,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import springboot.domains.CompanyRest;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -18,9 +20,18 @@ public class ScheduleUpdater {
 
     private final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 
+    private final RestTemplate restTemplate;
+
+    public ScheduleUpdater(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
     @Scheduled(fixedRate = 5000)
     public void reportCurrentTime() {
         logger.info("The time is now {}", dateFormat.format(new Date()));
+        CompanyRest s = restTemplate.getForObject("https://www.alphavantage.co/", CompanyRest.class);
+//        logger.info("This is company Rest", s);
+        System.out.println(s);
     }
 
 }
