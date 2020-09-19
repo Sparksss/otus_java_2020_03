faf109157f916c0b5c04421b91754750af41539a
Ilya Rogatkin <toch.in.go@googlemail.com>
2020-09-20 00:00:01 +0300

    add rest remplate for requests in cron

--- stock-analyzer/src/main/java/springboot/Application.java
+++ stock-analyzer/src/main/java/springboot/Application.java
@@ -2,7 +2,10 @@ package springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * Created by ilya on Sep, 2020
@@ -12,5 +15,10 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
     }
 }
