8afad6e6e7a085f1b19ef429008b46646afca82f
Ilya Rogatkin <toch.in.go@googlemail.com>
2020-09-26 19:18:31 +0300

    add symbol property in company entity

--- stock-analyzer/src/main/java/springboot/domains/Company.java
+++ stock-analyzer/src/main/java/springboot/domains/Company.java
@@ -1,18 +1,18 @@
 package springboot.domains;
 
 import lombok.Data;
-import lombok.RequiredArgsConstructor;
 
 /**
  * Created by ilya on Sep, 2020
  */
 @Data
-@RequiredArgsConstructor
 public class Company {
 
     private long id;
 
     private String name;
+
+    private String symbol;
 
     private String description;
 
