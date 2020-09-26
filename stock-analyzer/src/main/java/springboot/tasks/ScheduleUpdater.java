178a747d9e169ad87a965a2518df5e1e2eba73d5
Ilya Rogatkin <toch.in.go@googlemail.com>
2020-09-26 14:03:23 +0300

    add adapter int ScheduleUpdater

--- stock-analyzer/src/main/java/springboot/tasks/ScheduleUpdater.java
+++ stock-analyzer/src/main/java/springboot/tasks/ScheduleUpdater.java
@@ -5,7 +5,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
-import springboot.domains.CompanyDataRest;
 import springboot.services.ExtractCompanyData;
 
 import java.text.SimpleDateFormat;
@@ -34,9 +33,8 @@ public class ScheduleUpdater {
     @Scheduled(fixedRateString = "${schedule.fixedRate}")
     public void reportCurrentTime() {
         logger.info("The time is now {}", dateFormat.format(new Date()));
-        Map<String, Map> s = restTemplate.getForObject("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=AAPL&apikey=S1RP7VOYRC84TD3Z", Map.class);
-        CompanyDataRest companyDataRest = extractCompanyData.extract(s);
-        System.out.println(companyDataRest.getClosePrice());
+        Map<String, Map> data = restTemplate.getForObject("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=AAPL&apikey=S1RP7VOYRC84TD3Z", Map.class);
+        extractCompanyData.save(data);
     }
 
 }
