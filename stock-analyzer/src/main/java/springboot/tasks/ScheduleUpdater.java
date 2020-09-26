53dd6fd3b4c99e1a656b3ae9720e02e6a12fd9d9
Ilya Rogatkin <toch.in.go@googlemail.com>
2020-09-26 19:21:35 +0300

    refactoring URL params add save data

--- stock-analyzer/src/main/java/springboot/tasks/ScheduleUpdater.java
+++ stock-analyzer/src/main/java/springboot/tasks/ScheduleUpdater.java
@@ -5,10 +5,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+import springboot.dao.CompanyDao;
+import springboot.domains.Company;
+import springboot.parameters.Periods;
 import springboot.services.ExtractCompanyData;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -19,22 +23,34 @@ public class ScheduleUpdater {
 
     private static final Logger logger = LoggerFactory.getLogger(ScheduleUpdater.class);
 
+    private final String URL = "https://www.alphavantage.co/query";
+
     private final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 
     private final RestTemplate restTemplate;
 
     private ExtractCompanyData extractCompanyData;
 
-    public ScheduleUpdater(RestTemplate restTemplate, ExtractCompanyData extractCompanyData) {
+    private CompanyDao companyDao;
+
+    private StringBuilder preparedURLWithParams = new StringBuilder();
+
+    public ScheduleUpdater(RestTemplate restTemplate, ExtractCompanyData extractCompanyData, CompanyDao companyDao) {
         this.restTemplate = restTemplate;
         this.extractCompanyData = extractCompanyData;
+        this.companyDao = companyDao;
     }
 
     @Scheduled(fixedRateString = "${schedule.fixedRate}")
     public void reportCurrentTime() {
         logger.info("The time is now {}", dateFormat.format(new Date()));
-        Map<String, Map> data = restTemplate.getForObject("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=AAPL&apikey=S1RP7VOYRC84TD3Z", Map.class);
-        extractCompanyData.save(data);
+        List<Company> companies = companyDao.getAll();
+        for(Company company : companies) {
+            preparedURLWithParams.append(URL + "?function=" + Periods.valueOf("DAILY") + "&symbol=" + company.getSymbol() + "apikey=S1RP7VOYRC84TD3Z");
+            Map<String, Map> data = restTemplate.getForObject(preparedURLWithParams.toString(), Map.class);
+            extractCompanyData.save(data);
+            preparedURLWithParams.setLength(0);
+        }
     }
 
 }
