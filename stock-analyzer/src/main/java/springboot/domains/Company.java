3f7ae975e9bc6bf2fc1a2d3ccac8bc4904bf8bb7
Ilya Rogatkin <toch.in.go@googlemail.com>
2020-09-26 13:59:54 +0300

    use lombo for entity

--- stock-analyzer/src/main/java/springboot/domains/Company.java
+++ stock-analyzer/src/main/java/springboot/domains/Company.java
@@ -1,8 +1,13 @@
 package springboot.domains;
 
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
 /**
  * Created by ilya on Sep, 2020
  */
+@Data
+@RequiredArgsConstructor
 public class Company {
 
     private long id;
@@ -11,36 +16,4 @@ public class Company {
 
     private String description;
 
-    public Company() {
-    }
-
-    public Company(long id, String name, String description) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
