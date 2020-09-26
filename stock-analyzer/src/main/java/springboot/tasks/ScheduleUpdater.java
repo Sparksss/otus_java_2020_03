a75ab13dfb538060e9a7ef793c8b2f1bd0c22aca
Ilya Rogatkin <toch.in.go@googlemail.com>
2020-09-26 20:01:46 +0300

    add config for get properties

--- stock-analyzer/src/main/java/springboot/tasks/ScheduleUpdater.java
+++ stock-analyzer/src/main/java/springboot/tasks/ScheduleUpdater.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+import springboot.config.AlphavantageConf;
 import springboot.dao.CompanyDao;
 import springboot.domains.Company;
 import springboot.parameters.Periods;
@@ -21,6 +22,8 @@ import java.util.Map;
 @Component
 public class ScheduleUpdater {
 
+    private AlphavantageConf alphavantageConf;
+
     private static final Logger logger = LoggerFactory.getLogger(ScheduleUpdater.class);
 
     private final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
@@ -33,18 +36,19 @@ public class ScheduleUpdater {
 
     private StringBuilder preparedURLWithParams = new StringBuilder();
 
-    public ScheduleUpdater(RestTemplate restTemplate, ExtractCompanyData extractCompanyData, CompanyDao companyDao) {
+    public ScheduleUpdater(RestTemplate restTemplate, ExtractCompanyData extractCompanyData, CompanyDao companyDao, AlphavantageConf alphavantageConf) {
         this.restTemplate = restTemplate;
         this.extractCompanyData = extractCompanyData;
         this.companyDao = companyDao;
+        this.alphavantageConf = alphavantageConf;
     }
 
     @Scheduled(fixedRateString = "${schedule.fixedRate}")
     public void reportCurrentTime() {
         logger.info("Next run scheduler {}", dateFormat.format(new Date()));
         List<Company> companies = companyDao.getAll();
-        String apiKey = "${schedule.alphavantage.apikey}";
-        String url = "${schedule.alphavantage.url}";
+        String apiKey =  this.alphavantageConf.getApikey();
+        String url = this.alphavantageConf.getUrl();
         for(Company company : companies) {
             preparedURLWithParams.append(url + "?function=" + Periods.valueOf("DAILY") + "&symbol=" + company.getSymbol() + "&apikey=" + apiKey);
             Map<String, Map> data = restTemplate.getForObject(preparedURLWithParams.toString(), Map.class);
